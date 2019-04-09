a0e8b54cbbcb9fec575de2d8817c77688df8899c
Brandon Castillo <brandonmaerina@gmail.com>
2019-04-09 01:43:58 -0700

    Add website images. will delete excess later

--- static/js/articles.js
+++ static/js/articles.js
@@ -10,16 +10,40 @@ $(document).ready(function(){
         let published = response.articles[i].publishedAt;
         let content = response.articles[i].content;
         $('.api-container').append(`
-        <div class="columns">
-        <div class="column is-two-thirds">
-        <h1 class="title">${title}</h1>
-        <h3 class="author">${author}</h3>
-        <h3><a href="${url}">${url}</a></h3>
-        <h4 class="time">${published}</h4>
-
-        </div>
-        </div>
+        <div class="box article-box is-one-third">
+        <article class="media">   
+          <div class="media-content">
+            <div class="content">         
+              <h1 class="title is-3 article-title">${title}</h1>
+              <h3 class="author article-author">${author}</h3>
+                <h3><a href="${url}">${url}</a></h3>
+                <h4 class="time article-time">${published}</h4>
+            </div>
+            <nav class="level is-mobile">
+              <div class="level-left">
+                <a class="level-item" aria-label="reply">
+                  <span class="icon is-small">
+                    <i class="fas fa-reply" aria-hidden="true"></i>
+                  </span>
+                </a>
+                <a class="level-item" aria-label="retweet">
+                  <span class="icon is-small">
+                    <i class="fas fa-retweet" aria-hidden="true"></i>
+                  </span>
+                </a>
+                <a class="level-item" aria-label="like">
+                  <span class="icon is-small">
+                    <i class="fas fa-heart" aria-hidden="true"></i>
+                  </span>
+                </a>
+              </div>
+            </nav>
+          </div>
+        </article>
+      </div>
         `);
         }      
       }});
   });
+
+ 
