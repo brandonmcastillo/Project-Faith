53e5200295a99c693d6ab40d86b4ae501b972c48
Brandon Castillo <brandonmaerina@gmail.com>
2019-04-11 00:28:47 -0700

    favicon route

--- app.py
+++ app.py
@@ -41,6 +41,11 @@ def after_request(response):
   g.db.close()
   return response
 
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, 'static'),
+                               'favicon.ico', mimetype='image/png')
+
 @app.route('/')
 def index():
     return render_template('landing.html')
