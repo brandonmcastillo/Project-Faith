d80c1f6116586aab91842b25fecf52c840921be4
Brandon Castillo <brandonmaerina@gmail.com>
2019-04-11 02:29:19 -0700

    another heroku test

--- app.py
+++ app.py
@@ -285,7 +285,6 @@ if 'ON_HEROKU' in os.environ:
 
 app.run(debug=DEBUG, port=PORT)
 if __name__ == '__main__':
-    models.initialize()
-        import os  
+        models.initialize() 
         port = int(os.environ.get('PORT', 33507)) 
         app.run(host='0.0.0.0', port=port)
